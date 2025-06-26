233c26d73c57bba61bfcdec0e69f7a3b2443fc5c
sachin bagaria <sachinbagariaofficial@gmail.com>
2025-06-26 12:43:07 -0500

    add analaytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { PromptProvider } from "@/context/PromptContext";
-
+import { Analytics } from "@vercel/analytics/next";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -60,6 +60,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <PromptProvider>{children}</PromptProvider>
+        <Analytics />
       </body>
     </html>
   );
